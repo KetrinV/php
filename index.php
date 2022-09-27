72350696ebff9221cd3300941ec4587c16f6dfbb
KetrnV <ketrnvanatoa@gmail.com>
2022-09-27 10:43:43 +0300

    message

--- index.php
+++ index.php
@@ -4,12 +4,4 @@ require_once('connection.php');
 
 echo '<ul>';
 
-$stmt = $pdo->query('SELECT * FROM books');
-while ($row = $stmt->fetch())
-{
-    echo '<li><a href="book.php?id=' . $row['id'] . '">' . $row['title'] . '</li>';
-}
-
-echo '</ul>';
-
 
