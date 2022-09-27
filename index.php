b7792aa15e9259748fca8a6e819ed4cdde582c17
KetrnV <ketrnvanatoa@gmail.com>
2022-09-27 10:23:30 +0300

    njnjoi

--- index.php
+++ index.php
@@ -1,19 +1,15 @@
 <?php
 
-require_once('config.php');
-
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
-try {
-     $pdo = new PDO($dsn, $user, $pass, $options);
-} catch (\PDOException $e) {
-     throw new \PDOException($e->getMessage(), (int)$e->getCode());
-}
+require_once('connection.php');
 
 echo '<ul>';
+
+$stmt = $pdo->query('SELECT * FROM books');
+while ($row = $stmt->fetch())
+{
+    echo '<li><a href="book.php?id=' . $row['id'] . '">' . $row['title'] . '</li>';
+}
+
+echo '</ul>';
 
 
