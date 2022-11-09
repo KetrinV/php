91bcdec48bcd69ccf90f7c694d080b0a3a319d70
KetrnV <ketrnvanatoa@gmail.com>
2022-11-09 12:10:04 +0200

    add authors change

--- edit.php
+++ edit.php
@@ -5,25 +5,30 @@ require_once('connection.php');
 $id = $_GET['id'];
 
 if ( isset($_POST['edit']) && $_POST['edit'] == 'Salvesta' ) {
-    // var_dump($_POST);
+    $stmt = $pdo->prepare('UPDATE books SET title = :title, stock_saldo = :stock_saldo, price = :price, pages = :pages, summary = :summary,  cover_path  = :cover_path, type  = :type, language  = :language  WHERE id = :id');
+    $stmt->execute(['title' => $_POST['title'], 'stock_saldo' => $_POST['stock-saldo'], 'price' => str_replace(',', '.', $_POST['price']), 'pages' => $_POST['pages'], 'summary' => $_POST['summary'], 'cover_path' => $_POST['cover_path'], 'type' => $_POST['type'], 'language' => $_POST['language'], 'id' => $id]); //nt str replace asendab koma punktiga
 
-    $stmt = $pdo->prepare('UPDATE books SET title = :title, stock_saldo = :stock_saldo WHERE id = :id');
-    $stmt->execute(['title' => $_POST['title'], 'stock_saldo' => $_POST['stock-saldo'], 'id' => $id]);
+    $stmt = $pdo->prepare('UPDATE book_authors SET author_id = :author_id  WHERE book_id = :book_id');
+    $stmt->execute(['author_id' => $_POST['author_id'], 'book_id' => $id]);
 
     header('Location: book.php?id=' . $id);
 }
 
-$stmtBook = $pdo->prepare('SELECT * FROM books WHERE id = :id');
+$stmtBook = $pdo->prepare('SELECT * FROM books b LEFT JOIN book_authors ba ON b.id=ba.book_id WHERE b.id = :id');
 $stmtBook->execute(['id' => $id]);
 $book = $stmtBook->fetch();
 
-$stmtBookAuthors = $pdo->prepare('SELECT * FROM book_authors ba LEFT JOIN authors a ON a.id=ba.author_id  WHERE ba.book_id = :book_id');
-$stmtBookAuthors->execute(['book_id' => $id]);
+// var_dump($book);
 
-$stmtAuthors = $pdo->prepare('SELECT * FROM authors WHERE id NOT IN (SELECT author_id FROM book_authors WHERE book_id = :book_id)');
-$stmtAuthors->execute(['book_id' => $id]);
+// $stmtBookAuthors = $pdo->prepare('SELECT * FROM book_authors ba LEFT JOIN authors a ON a.id=ba.author_id  WHERE ba.book_id = :book_id');
+// $stmtBookAuthors->execute(['book_id' => $id]);
 
+$stmtAuthors = $pdo->query('SELECT * FROM authors ORDER BY first_name, last_name');
+
+$stmtLanguages = $pdo->query('SELECT distinct language FROM books');
+// $stmtAuthors->execute(['book_id' => $id]);
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -31,35 +36,36 @@ $stmtAuthors->execute(['book_id' => $id]);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20,400,0,0" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
     <title><?=$book['title'];?></title>
 </head>
 <body>
-
+    
 <form action="edit.php?id=<?=$id;?>" method="POST">
-    <label for="title">Title:</label> <input type="text" name="title" value="<?=$book['title'];?>" style="width: 320px;">
+    <label for="title">Pealkiri:</label> <input type="text" name="title" value="<?=$book['title'];?>" style="width: 320px;">
     <br>
-    <label for="title">Stock saldo:</label> <input type="text" name="stock-saldo" value="<?=$book['stock_saldo'];?>">
+
+    <label for="title">Hind (€):</label> <input type="text" name="price" value="<?=number_format(round($book['price'], 2), 2, ',');?>">
     <br>
-    <div style="font-weight: bold;">Authors</div>
-    <select name="authors" id="author-dd">
-        <option value=""></option>
+
+    <label for="title">Laos:</label> <input type="text" name="stock-saldo" value="<?=$book['stock_saldo'];?>">
+    <br>
+
+    <br>
+    <br>
+
+
+    <div>Muuda autorit:</div>
+    <select name="author_id">
         <?php while ($author = $stmtAuthors->fetch()) { ?>
-            <option value="<?=$author['id'];?>"><?=$author['first_name'];?> <?=$author['last_name'];?></option>
+            <option value="<?=$author['id'];?>" <?=$author['id'] == $book['author_id'] ? 'selected' : '';?>>
+                <?=$author['first_name'];?> <?=$author['last_name'];?>
+            </option>
         <?php } ?>
     </select>
-    <br>
-    <?php while ($bookAuthor = $stmtBookAuthors->fetch()) { ?>
-        <div class="author-row">
-            <span class="author-name">
-                <?=$bookAuthor['first_name'];?> <?=$bookAuthor['last_name'];?>
-            </span>
-            <span class="material-symbols-outlined" style="font-size: 16px; vertical-align: text-bottom;">delete</span>
-            <input class="author-id" type="hidden" name="author[]" value="<?=$bookAuthor['id'];?>">
-        </div>
-    <?php } ?>
+
     <input type="submit" value="Salvesta" name="edit">
 </form>
-<script src="app.js"></script>
+ <script src="app.js"></script>
 </body>
 </html>
